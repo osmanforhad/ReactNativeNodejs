e21b0bbec1db337ca74fb4273ac9956ed47121bc
osmanforhad <osmanforhad93@gmail.com>
2021-02-17 14:56:37 +0600

    working with card component and styling them

--- newsApp/App.js
+++ newsApp/App.js
@@ -2,36 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+{/*import the Developer Created component */}
+import Header from './src/components/Header';
+import Card from './src/components/Card';
+
 export default function App() {
   return (
-    <View style={{
-      backgroundColor: 'silver',
-       padding:50,
-       flex:1,
-       flexDirection: 'row',
-       justifyContent: 'space-around',
-       alignItems: 'flex-start',
-      }}>
-    {/*making a flex box*/}
-      <View style={{width:60, height:60, backgroundColor: 'red'}}>
-        <Text>A</Text>
-      </View>
-      <View style={{width:60, height:60, backgroundColor: 'green'}}>
-        <Text>B</Text>
-      </View>
-      <View style={{width:60, height:60, backgroundColor: 'blue'}}>
-        <Text>C</Text>
-      </View>
-      <StatusBar style="auto" />
+    <View>
+    
+    {/*include the Developer Created Component */}
+      <Header/>
+      <Card/>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
---
+++ newsApp/src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {StyleSheet, Text, View, Platform} from 'react-native'
+
+{/*start functional component*/}
+const Header = () => {
+    return(
+        <View style={styles.header}>
+    <Text style={styles.title}>News App</Text>
+    </View>
+    );
+}
+{/*end of the functional component*/}
+
+const styles = StyleSheet.create({
+    header:{
+        backgroundColor: Platform.OS === 'android' ? '#72bcd4' : '#ffffff',
+        padding:15,
+        borderBottomColor: Platform.OS === "android" ? '#ffffff' : '#72bcd4',
+        borderBottomWidth:1
+    },
+    title: {
+        marginTop: 40,
+        textAlign: 'center',
+        color: Platform.OS === 'android' ? '#ffffff' : '#72bcd4'
+    }
+})
+
+{/*export component for output */}
+export default Header;
