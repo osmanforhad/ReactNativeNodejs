7addc8b6c41ccd93d8bdf89912e3edbd4ceb5391
osmanforhad <osmanforhad93@gmail.com>
2021-02-18 12:22:11 +0600

    remove unnesessary and not activate code

--- newsApp/App.js
+++ newsApp/App.js
@@ -1,34 +1,13 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
-//import AppLoading from 'expo-app-loading';
-//import {AppLoading} from 'expo'
 
 {/*import the Developer Created component */}
 import Header from './src/components/Header';
 import Card from './src/components/Card';
 
-{/*function for load the font */}
-const loadFonts = () => {
-  return Font>loadAsync({
-    'Ubuntu': require('./assets/fonts/Ubuntu-Regular.ttf'),
-    'Ubuntu-Bold': require('./assets/fonts/Ubuntu-Bold.ttf')
-  });
-}
-
 {/*export component for output */}
 export default function App() {
-
-  // const [fontLoaded, setFontLoaded] = useState(false);
-  // if(!fontLoaded){
-  //   return(
-  //     <AppLoading
-  //         startAsync={this.loadFonts}
-  //         onFinish={() => this.setFontLoaded({ isReady: true })}
-  //         onError={console.warn}
-  //       />
-  //   );
-  // }
 
   return (
     <View>
