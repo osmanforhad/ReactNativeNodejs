be0c6cb9a2bf7b73026c5ebf3502c2ce27096b38
osmanforhad <osmanforhad93@gmail.com>
2021-02-23 17:38:58 +0600

    working with dispatch redux action

--- newsApp/src/screens/NewsListScreen.js
+++ newsApp/src/screens/NewsListScreen.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StyleSheet} from 'react-native';
+import {useSelector, useDispatch} from 'react-redux';
 
 {/*import the Developer Created component */}
 import Card from '../components/Card';
+import * as newsAction from '../redux/actions/newsAction';
 
 {/*start function for NewsList */}
 const NewsListScreen = props =>{
+
+
+    {/*initialize the dispatch */}
+    const dispatch = useDispatch();
+
+    {/*use effect hook function for render */}
+    useEffect(() => {
+        dispatch(newsAction.fetchArticles())
+    }, [dispatch]);
+
+     {/*select the state which need right now */}
+    const articles = useSelector(state => state.news.articles);
+    console.log(articles);
   
     return(
       
