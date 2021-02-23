6dee1e23f8804f298e3ba4599c31e809604c69a2
osmanforhad <osmanforhad93@gmail.com>
2021-02-23 15:40:08 +0600

    Working with Drawer Navigator

--- newsApp/src/navigation/AppNavigator.js
+++ newsApp/src/navigation/AppNavigator.js
@@ -1,22 +1,40 @@
 import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import {MaterialIcons} from '@expo/vector-icons';
 
 {/*import the Developer Created Component */}
 import NewsListScreen from '../screens/NewsListScreen';
 import NewsDetailsScreen from '../screens/NewsDetailsScreen';
 import FavoritesScreen from '../screens/FavoritesScreen';
+import AboutScreen from '../screens/AboutScreen';
 
 {/*Initialize the Navigator as Constant */}
 const Stack = createStackNavigator();
 const Tabs = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+{/*function for Left navigation Drawer Icons Bar */}
+const HeaderLeft = () => {
+    const navigation = useNavigation();
+
+    return(
+        <MaterialIcons name="menu" size={24} onPress={() => {navigation.openDrawer()
+        
+        }} />
+    );
+}//end function for left navigation Drawer Icon Bar
 
 {/*function for Home Navigation */}
 function HomeNavigator() {
     return(
-        <Stack.Navigator>
+        <Stack.Navigator
+        screenOptions={{
+            headerLeft: () => <HeaderLeft />
+        }}
+        >
         <Stack.Screen 
         name="NewsList" 
         component={NewsListScreen}
@@ -34,33 +52,60 @@ function HomeNavigator() {
 {/*function for Favorites navigation */}
 function FavoritesNavigator() {
     return(
-        <Stack.Navigator>
+        <Stack.Navigator
+         screenOptions={{
+            headerLeft: () => <HeaderLeft />
+        }}
+        >
             <Stack.Screen name="Favorites" component={FavoritesScreen} />
         </Stack.Navigator>
     );
 }//end of the FavoritesNavigator
 
+{/*function for About navigation */}
+function AboutNavigator() {
+    return(
+        <Stack.Navigator
+         screenOptions={{
+            headerLeft: () => <HeaderLeft />
+        }}
+        >
+            <Stack.Screen name="About" component={AboutScreen} />
+        </Stack.Navigator>
+    );
+}//end of the About Screen Navigation
+
+{/* function for Tabs navigation*/}
+function TabsNavigator() {
+    return(
+        <Tabs.Navigator
+        screenOptions={({route}) => ({
+            tabBarIcon:() => {
+                let iconName;
+                if(route.name=="Home"){
+                    iconName = "home"
+                } else if(route.name=="Favorites"){
+                    iconName = "favorite"
+                }
+
+                return <MaterialIcons name={iconName} size={24} />
+            }
+        })}
+        >
+            <Tabs.Screen name="Home" component={HomeNavigator} />
+            <Tabs.Screen name="Favorites" component={FavoritesNavigator} />
+        </Tabs.Navigator>
+    );
+}//end of the TabsNavigator function
+
 {/*Define Navigator function */}
 function AppNavigator() {
     return(
         <NavigationContainer>
-           <Tabs.Navigator
-           screenOptions={({route}) => ({
-               tabBarIcon:() => {
-                   let iconName;
-                   if(route.name=="Home"){
-                       iconName = "home"
-                   } else if(route.name=="Favorites"){
-                       iconName = "favorite"
-                   }
-
-                   return <MaterialIcons name={iconName} size={24} />
-               }
-           })}
-           >
-               <Tabs.Screen name="Home" component={HomeNavigator} />
-               <Tabs.Screen name="Favorites" component={FavoritesNavigator} />
-           </Tabs.Navigator>
+          <Drawer.Navigator>
+              <Drawer.Screen name="News" component={TabsNavigator} />
+              <Drawer.Screen name="About" component={AboutNavigator} />
+          </Drawer.Navigator>
         </NavigationContainer>
     );
 }
