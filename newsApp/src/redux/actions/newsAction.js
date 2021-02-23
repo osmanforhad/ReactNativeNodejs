d3580efb8e3b6b5bdbba2a885045e99bef379cee
osmanforhad <osmanforhad93@gmail.com>
2021-02-23 17:19:25 +0600

    working with Redux HTTP request and News API

--- newsApp/src/redux/actions/newsAction.js
+++ newsApp/src/redux/actions/newsAction.js
@@ -4,8 +4,16 @@ export const ADD_FAVORITES = "ADD_FAVORITES";
 
 {/* function for export object */}
 export const fetchArticles = () => {
-    return {
-        type: FETCH_ARTICLES,
-        payload: {id: 1, title: 'Sport News', description: 'Sports News is On'}
+    return async dispatch => {
+
+        //logic to fetch data
+       const result = await fetch('http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=ebd49c72d60847ae8925c7a6d6e20c0f');
+
+       const resultData = await result.json();
+
+        dispatch({
+            type: FETCH_ARTICLES,
+        payload: resultData
+        });
     }
 }//end of the fetchArticles function
