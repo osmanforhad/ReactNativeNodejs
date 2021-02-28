b9ab82e96abcf5eddaecab27cd7ca4776fee3cea
Osman Forhad <osmanforhad93@gmail.com>
2021-02-28 15:59:17 +0600

    working with news details screen and finish the news app

--- newsApp/src/screens/NewsDetailsScreen.js
+++ newsApp/src/screens/NewsDetailsScreen.js
@@ -1,19 +1,85 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, Text, ImageBackground } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import {useSelector, useDispatch} from 'react-redux';
 
-{/*start function for NewsDetails */}
-const NewsDetailsScreen = () =>{
-    return(
-<View>
-    <Text>News Details Screen</Text>
-</View>
-    );
-}//end of the NewsDetails function
+{/*import the developer created component */}
+import * as newsAction from '../redux/actions/newsAction';
 
-{/*style code */}
+{ /*start function for NewsDetails */ }
+const NewsDetailsScreen = props => {
+
+    const dispatch = useDispatch();
+
+    const {articleUrl} = props.route.params;
+    const article = useSelector(state => state.news.articles.articles.find(article => article.url === articleUrl));
+
+    {/*define for fill favorite icon or not */}
+    const isFav = useSelector(state => state.news.favorites.some(article => article.url === article.url));
+
+        return ( 
+            <View style ={styles.container}>
+            <View style = { styles.heading } >
+            <Text style = { styles.title } >{article.title}</Text> 
+            </View> 
+            <View>
+            <ImageBackground source={{uri: article.urlToImage}} style = { styles.image } >
+            <View style={styles.titleContainer}>
+            <Text style = {styles.author } >{article.author}</Text> 
+            </View>
+            <MaterialIcons
+         name={isFav ? 'favorite' : 'favorite-border'} 
+         color="#72bcd4" 
+         size={24}
+             onPress={() => {
+                 dispatch(newsAction.toggleFavorites(article.url))
+             }}
+         />
+            </ImageBackground> 
+            </View> 
+            <View style = { styles.description } >
+            <Text style = { styles.descriptionText } >{article.description}</Text>  
+            </View> 
+            </View>
+        );
+    } //end of the NewsDetails function
+
+{ /*style code */ }
 const styles = StyleSheet.create({
-    
-});//end of the style code
+    container: {
+        flex: 1,
+        marginVertical: 20
+    },
+    heading: {
+        marginHorizontal: 20,
+        marginBottom: 10
+    },
+    title: {
+        fontSize: 24
+    },
+    image: {
+        width: '100%',
+        height: 200,
+        justifyContent: 'flex-end'
+    },
+    titleContainer: {
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    author: {
+        fontSize: 20,
+        color: 'white'
+    },
+    description: {
+        margin: 10
+    },
+    descriptionText: {
+        fontSize: 20
+    }
+}); //end of the style code
 
-{/*export component function for output */}
+{ /*export component function for output */ }
 export default NewsDetailsScreen;
