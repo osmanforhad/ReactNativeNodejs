d264f37bb4a8ba962294da48fcd68b8564a3f778
Osman Forhad <osmanforhad93@gmail.com>
2021-03-02 14:50:25 +0600

    working with node js file system

--- nodeApp/app.js
+++ nodeApp/app.js
@@ -1,9 +1,11 @@
-//include the developer created file
-const arithmetic = require('./arithmetic');
+//file system
+const fs = require('fs');
 
-//declare a variable nad call the function and pass the paramiter
-numbers = arithmetic.addNumbers(4, 6);
+fs.readFile('./test.txt', (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
 
-console.log('The result is:' + numbers);
-
-console.log(arithmetic.oddEvenChecker('So : ' + numbers));
+    console.log(data.toString());
+})
