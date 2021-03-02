0b4f9229123907ea06adc6b5bec4629e881b402a
Osman Forhad <osmanforhad93@gmail.com>
2021-03-02 15:50:34 +0600

    working with node js event

--- nodeApp/app.js
+++ nodeApp/app.js
@@ -1,11 +1,25 @@
+//define the event class
+const EventEmitter = require('events');
+
+//cretae the instance of the event class
+const emitter = new EventEmitter();
+
+//register an event
+emitter.on('greeting', (arg) => {
+    console.log('This is the event class', arg.name);
+});
+
+//triger the event
+emitter.emit('greeting', { name: 'osman forhad' });
+
 //file system
-const fs = require('fs');
+// const fs = require('fs');
 
-fs.readFile('./test.txt', (err, data) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
+// fs.readFile('./test.txt', (err, data) => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
 
-    console.log(data.toString());
-})
+//     console.log(data.toString());
+// })
