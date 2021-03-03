cd3f1ccf88251927db95e9dc2db6145d8b525e48
Osman Forhad <osmanforhad93@gmail.com>
2021-03-03 14:43:24 +0600

    working with node js env file

--- home-listing/server/index.js
+++ home-listing/server/index.js
@@ -4,6 +4,9 @@ const express = require('express');
 //initial the express package
 const app = express();
 
+//include .env file
+require('dotenv').config();
+
 //define the get express request server
 app.get('/', (req, res) => {
     res.send('Wellcome to Express');
@@ -14,5 +17,13 @@ app.get('/api/listing', (req, res) => {
     res.send([{ id: 1, roomType: "Duplex" }, { id: 2, roomType: 'Flat' }]);
 })
 
+/**
+ * excess the env file with specific variable
+ * and if not deinfe port in the env
+ * 
+ * it will excess the default port
+ */
+const port = process.env.PORT || 3000
+
 //export the server out put
-app.listen(3000, () => console.log('Server is running on port 3000'));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
