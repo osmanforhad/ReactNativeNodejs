fed6ccca9f09519f46042be719b1f91c82617850
Osman Forhad <osmanforhad93@gmail.com>
2021-03-03 12:52:52 +0600

    working with node js http module

--- nodeApp/app.js
+++ nodeApp/app.js
@@ -3,8 +3,21 @@ const http = require('http');
 
 //create server
 const server = http.createServer((req, res) => {
+
+    //defien an array
+    const movies = [
+        { id: 1, name: 'Titanic' },
+        { id: 2, name: 'Early Bird' }
+    ]
+
     if (req.url == '/') {
         res.write('Welcome to the http module');
+        res.end();
+    }
+
+    //setup the routes
+    if (req.url == '/api/movies') {
+        res.write(JSON.stringify(movies));
         res.end();
     }
 });
