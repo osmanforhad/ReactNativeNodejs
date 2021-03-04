1db8ffea20c9396231d8be5b28a5758c4f09e34e
Osman Forhad <osmanforhad93@gmail.com>
2021-03-04 15:38:29 +0600

    working with create an database schema

---
+++ nodeApp/models/House.js
@@ -0,0 +1,13 @@
+//import mongooes
+const mongoose = require('mongoose');
+
+//define the schema
+const HouseSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    address: { type: String, required: true },
+    homeType: String,
+    description: String,
+    price: { type: Number, required: true },
+    image: String,
+    yearBuilt: Number
+})
